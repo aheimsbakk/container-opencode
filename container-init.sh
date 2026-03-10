b730d20132dd828a940f22b3745df81f9fb24b40
Arnulf Heimsbakk <8190865+aheimsbakk@users.noreply.github.com>
2026-03-10 15:52:08 +0100

    undo, undo, undoo

--- container-init.sh
+++ container-init.sh
@@ -85,7 +85,4 @@ fi
 
 # Execute the supplied command (defaults to a shell when no explicit command is given).
 # Run in background and trap signals so CTRL+C (SIGINT/SIGTERM) gracefully stops the process.
-"${@:-/bin/bash}" &
-CHILD_PID=$!
-trap 'kill -TERM "$CHILD_PID" 2>/dev/null' SIGINT SIGTERM
-wait "$CHILD_PID"
+exec "${@:-/bin/bash}"
